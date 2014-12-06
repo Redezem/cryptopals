89fae2fd32c43ce76e37bffdc2ec7b89b06814ec
Redezem <redezem@gmail.com>
2014-12-06 21:52:35 +0800

    A Bunch of stuff

---
+++ assign1/RepeatingXorBreaker.py
@@ -0,0 +1,15 @@
+import base64
+import binascii
+import sys
+
+firstString = sys.argv[1]
+secondString = sys.argv[2]
+
+if len(firstString) == len(secondString):
+	print "hamming it up..."
+	i = 0
+	while i < len(firstString):
+		#Right let's ham some ham.
+		
+else:
+	print "Well I guess you suck"
--- assign1/repeatingBitCypher.py
+++ assign1/repeatingBitCypher.py
@@ -1,3 +1,36 @@
-
 import sys
+import binascii
 
+fileName = sys.argv[1]
+codeWord = sys.argv[2]
+
+finalString = ""
+
+opendFile = open(fileName,"r")
+
+fileContents = ""
+
+for line in opendFile:
+	fileContents = fileContents+line 
+
+i = 0
+j = 0
+
+while i < len(fileContents):
+	while j < len(codeWord):
+		if i >= len(fileContents):
+			break
+		newcharacter = chr(ord(fileContents[i]) ^ ord(codeWord[j]))
+		finalString=finalString + newcharacter
+		j = j+1
+		i = i+1
+	j = 0
+#hopefully, that works
+
+finalString = binascii.hexlify(finalString)
+
+print "resulting in " + finalString
+
+exitfile = open("exit.txt","w")
+
+exitfile.write(finalString)
